b181cc765945bee598229301dabd38a68083ab22
Rusty Speedy <rusty@4iiz.com>
2020-09-08 16:35:22 -0400

    codebase updates as of 07/18/2020

---
+++ lib/scripts/mob/212/mordran.lua
@@ -0,0 +1,11 @@
+function onpulse_pc()
+  local case = number(0, 2)
+  if ((case == 0) and (number(0, 1) == 0)) then
+    say("Congratulations on passing the tribulations of the Right of Passage to proceed to Kir'Draxin type 'enter kir drax'in'")
+  elseif ((case == 1) and (number(0, 2) == 0)) then
+    say("Congratulations on passing the tribulations of the Right of Passage to return to the begining of it all type 'enter tablet'")
+  elseif ((case == 2) and (number(0, 2) == 0)) then
+    say("Oh before you make your choice and depart do remember to examine your certificate rewards may be hidden within.")
+    emote("stares directly at you, peering into your very soul and gauges when your soul will be within his eternal grasp.")
+    end
+end
--- lib/scripts/mob/never_die.lua
+++ lib/scripts/mob/never_die.lua
@@ -1,4 +1,7 @@
 function onpulse_all()
+-- The mob's hit points will be restored to maximum every pulse to prevent the
+-- mob from dieing. Attached to mob 19113.
+
   if (me.hp < me.maxhp) then
     me.hp = me.maxhp
   end
